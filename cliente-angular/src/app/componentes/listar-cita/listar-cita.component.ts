71bc161c35d8ca908bae161a06ab528c49a266d4
Nicolas Cunalata <nicolas.cunalata@espoch.edu.ec>
2020-07-29 22:37:09 -0500

    tabla interactiva del listado de citas

--- cliente-angular/src/app/componentes/listar-cita/listar-cita.component.ts
+++ cliente-angular/src/app/componentes/listar-cita/listar-cita.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {CitasService} from '../../servicios/citas.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-listar-cita',
   templateUrl: './listar-cita.component.html',
   styleUrls: ['./listar-cita.component.css']
 })
-export class ListarCitaComponent implements OnInit {
+export class ListarCitaComponent implements OnDestroy, OnInit {
 
+  dtOptions: DataTables.Settings = {};
+  dtTrigger = new Subject();
 
   citas:any=[];
   constructor(private service: CitasService) { }
 
   ngOnInit(): void {
+    
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json'
+      }
+    };
     this.listarCitas();
+
+  }
+  ngOnDestroy(){
+    this.dtTrigger.unsubscribe();
   }
   listarCitas(){
     this.service.getCitas()
@@ -21,6 +37,7 @@ export class ListarCitaComponent implements OnInit {
       data=>{
         console.log(data);
         this.citas=data;
+        this.dtTrigger.next();
     },
       err => 
       console.log(err)
